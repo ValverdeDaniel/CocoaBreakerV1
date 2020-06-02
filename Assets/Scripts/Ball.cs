8fe454e09ed851b0dee6f74d9793ff65e8bb1495
ValverdeDaniel <41589992+ValverdeDaniel@users.noreply.github.com>
2020-06-02 13:15:42 -0600

    initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,73 @@
+﻿
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.5f;
+
+    //state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    //cached component references
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //distance between paddle and ball
+        //transform.position exists for everything so you don't need to say GetComponent<>
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBallOnMouseClick();
+        }
+        
+    }
+    
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //this is how you access the rigidbody2d of Ball 
+            myRigidBody2D.velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        //positoin of the paddle
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        //transform.position is the current position of the ball  because we are within Ball
+        //we are setting the ball to be at the center ontop of the paddle and stick to it with the formula below
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocityTweak = new Vector2(Random.Range(0f, randomFactor), Random.Range(0f, randomFactor));
+        if(hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            myRigidBody2D.velocity += velocityTweak;
+        }
+        
+    }
+
+}
---
+++ Assets/Sprites/CandyCookie/readme.txt
@@ -0,0 +1,16 @@
+Match 3 - Candy Cookie Pack
+This is a mobile-friendly Candy cookie assets pack.
+It include more than 430 assets items.
+
+Features:
+- PNG files, resolution is 128x128;
+- 8 different shapes: square, round, leaf, heart, crescent, star, triangle, rhombus; 
+- 6 variant colour: blue, green, orange, purple, red, yellow;
+- 192 candies which have fruits;
+- 192 candies which have no fruits;
+- 48  other item: coins, diamonds, fruits, hearts, energies, bombs, clocks, chocolates,
+  waffles and so on. 
+- 4 ui backgrounds;
+- Perfect for your candy / cookie / yummy based games, such as match 3 games, 
+  candy quest, candy slide, candy removal, candy pop, candy eliminate, 
+  candy pickups and so on.
